200a5936ea43da0631f0dbb528425188a8fc1792
FAtigitub <166744176+FAtigitub@users.noreply.github.com>
2024-04-11 18:44:06 +0100

    Update VideoController.java

--- src/main/java/ma/xproce/tp2/dao/Controller/VideoController.java
+++ src/main/java/ma/xproce/tp2/dao/Controller/VideoController.java
@@ -11,47 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/videos")
+
 public class VideoController {
 
-    @Autowired
-    private VideoManager videoManager;
-
-    // Afficher la liste des vidéos
-    @GetMapping
-    public List<Video> getAllVideos() {
-        return videoManager.getAllVideos();
-    }
-
-    // Ajouter une nouvelle vidéo
-    @PostMapping
-    public ResponseEntity<Void> addVideo(@RequestBody Video video) {
-        videoManager.saveVideo(video);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    // Modifier une vidéo existante
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateVideo(@PathVariable Integer id, @RequestBody Video video) {
-        Video existingVideo = videoManager.getVideoById(id);
-        if (existingVideo != null) {
-            video.setId(id);
-            videoManager.saveVideo(video);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    // Supprimer une vidéo
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteVideo(@PathVariable Integer id) {
-        Video existingVideo = videoManager.getVideoById(id);
-        if (existingVideo != null) {
-            videoManager.deleteVideo(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
 }
